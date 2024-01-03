dca0b678faf5cef05dee572fe1403f6266140ea5
Thanujaavks <vksthanujaa@gmail.com>
2024-01-03 20:05:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import Layout from './layout/layout';
+import { Route, Routes } from 'react-router-dom';
+import { Home } from "./components/home/home";
+import { Tasks } from './components/tasks/tasks';
+import { Calendar } from './components/calendar/calendar';
+import { Analysis } from './components/analysis/analysis';
+import { Settings } from './components/settings/settings';
+import { Login } from './components/login/login';
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/tasks" element={<Tasks />} />
+        <Route path="/calendar" element={<Calendar />} />
+        <Route path="/analysis" element={<Analysis />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
